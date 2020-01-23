264227bfd15eaae4466611354ce52a5fda6ead70
Shreya Sahni <shreyasahni7@gmail.com>
2020-01-23 16:24:44 +0530

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -7,11 +7,22 @@ public class Driver {
 	String Name;
 	long Mobile;
 	int Licence;
-	public Cars Car=new Cars();
-	public void assignD(String nm, long mob, int lic, String mod, int no, float base, float stop) {
+	int Available;
+	Cars Car=new Cars();
+	public int isAvailable() {
+		return Available;
+	}
+	public Cars getCar() {
+		return Car;
+	}
+	public void assignD(String nm, long mob, int lic, String mod, int no, float base, float stop, int av) {
 		Name=nm;
 		Mobile=mob;
 		Licence=lic;
+		Available=av;
 		Car.assign(mod, no, base, stop);
+	}
+	public void display() {
+		System.out.println("Driver details:\nName: "+Name+"\nMobile Number: "+Mobile+"\nCar Model: "+Car.returnModel()+"\nCar Licence Plate Number: "+Car.returnPlate());
 	}
 }

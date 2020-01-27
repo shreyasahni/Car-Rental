456b2897f24ecaaa4eda8937e1602c899bc5f045
Shreya Sahni <shreyasahni7@gmail.com>
2020-01-28 01:22:36 +0530

    Update Cars.java

--- Cars.java
+++ Cars.java
@@ -3,7 +3,13 @@ package Cars;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Cars {
+interface Vehicle {
+	public void assign(String mod, int no, float base, float stop);
+	public String returnModel();
+	public int returnPlate();
+}
+
+public class Cars implements Vehicle {
 	String Model;
 	int PlateNo;
 	float BasePrice;
